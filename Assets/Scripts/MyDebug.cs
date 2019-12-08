81feccb69b9dfd394b2634e20d4072ae6ebbbff1
IamGroooooot <dury.ko@gmail.com>
2019-12-08 13:12:38 +0900

    .

--- Assets/Scripts/MyDebug.cs
+++ Assets/Scripts/MyDebug.cs
@@ -5,17 +5,17 @@ using UnityEngine.UI;
 
 public class MyDebug : MonoBehaviour
 {
-    private static Text text;
+    
+    //private static Text text;
 
     private void Awake()
     {
         //text = GameObject.Find("Debug").GetComponent<Text>();
-        
     }
-
+    
     public static void Log(string o)
     {
-        text.text = o;
+        //text.text = o;
     }
 
 }
